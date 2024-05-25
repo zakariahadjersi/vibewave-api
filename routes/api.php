8cb6af7ddbfbbeeb8bc87c4d21feed35ae58475b
Zakaria <zakarahadjersi@gmail.com>
2024-05-25 13:57:49 +0100

    latest posts endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('/post',[PostController::class, 'store']);
+Route::get('/posts',[PostController::class, 'getAllPosts']);
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
