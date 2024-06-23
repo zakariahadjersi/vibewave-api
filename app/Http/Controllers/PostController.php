f6c0c21a6610c7c485118a6ed2c48d64460868d6
Zakaria <zakarahadjersi@gmail.com>
2024-06-23 21:12:26 +0100

    fixed store fn

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,37 +7,43 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function store(Request $request){
-      
-        $validatedData = $request->validate([
-            'caption' => 'required|string',
-            'tags' => 'nullable|string',
-            'location' => 'nullable|string',
-            'file' => 'required|file|mimes:svg,jpeg,jpg', 
-        ]);
-        if ($request->hasFile('file')) {
-            $file = $request->file('file');
-            $filePath = $file->store('uploads');
-        } else {
-            return response()->json(['message' => 'File not found in request'], 400);
-        }
-        $user = auth()->user()->id;
+   public function store(Request $request)
+{
+    
+    $validatedData = $request->validate([
+        'caption' => 'required|string',
+        'tags' => 'nullable|string',
+        'location' => 'nullable|string',
+        'file' => 'required|file|mimes:svg,jpeg,jpg',
+    ]);
 
-        $post = new Post();
-        $post->caption = $validatedData['caption'];
-        $post->tags = $validatedData['tags'];
-        $post->location = $validatedData['location'];
-        $post->image_url = $filePath; 
-        $post->image_id = uniqid(); 
-        $post->creator = $user;
-        $post->save();
-
-        return response()->json(['message' => 'Post created successfully'], 201);
+    if ($request->hasFile('file')) {
+        
+        $file = $request->file('file');
+        $filePath = $file->store('uploads', 'public');
+    } else {
+        return response()->json(['message' => 'File not found in request'], 400);
     }
+
+    $user = auth()->user()->id;
+
+    
+    $post = new Post();
+    $post->caption = $validatedData['caption'];
+    $post->tags = $validatedData['tags'] ?? null;  
+    $post->location = $validatedData['location'] ?? null; 
+    $post->image_url = $filePath;
+    $post->image_id = uniqid();
+    $post->creator = $user;
+    $post->save();
+
+    return response()->json(['message' => 'Post created successfully', 'post' => $post], 201);
+}
+
 
     public function getAllPosts(){
       
-       $posts = Post::orderBy('created_at', 'desc')->take(20)->get();
+       $posts = Post::with('user:id,name')->orderBy('created_at', 'desc')->take(20)->get();
        return response()->json($posts);
     }
 }
